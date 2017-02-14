49b2e78277566a0936ec75a80b62cb633a51466b
Griffone <thegriffones@gmail.com>
2017-02-14 13:57:24 +0100

    Cleanup

--- main.c
+++ main.c
@@ -36,7 +36,7 @@
 extern void loop();
 
 int bLooping = 1;
-int x = 0, y = 0, dirX = 1, dirY = 1;
+int x = 1, y = 1, dirX = 1, dirY = 1;
 unsigned int lastTime = 0;
 
 // ===================
--- time.h
+++ time.h
@@ -7,10 +7,11 @@
 
 #include <pic32mx.h>
 
+// This will depend on the clock in question, for Uno32 board the clock is 80MHz
 #define PST_SINGLE_MS 80000
-// Feel free to redifine this to any non-zero multiple of 10
-// to calculate "fps" divide 1000 by this number
-#define PST_PERIOD_MS 20
+// Feel free to redifine this to any non-zero multiple of 2
+// to calculate ticks per second divede 1000 by this number
+#define PST_TICK_PERIOD_MS 20	// we use 20 to cap the frame rate at 50
 
 extern unsigned int time_tick;
 
