223c1868301c44f9788ad2278bf039c375d93b09
Griffone <thegriffones@gmail.com>
2017-02-25 13:38:06 +0100

    Implemented new highscore
    And also minor bugfixes

--- main.c
+++ main.c
@@ -38,6 +38,9 @@
 #define PS_STATE_SCORE	4
 #define PS_STATE_IDLE	5
 
+#define PS_BLINK_PERIOD	20
+#define PS_INPUT_PAD	20
+
 typedef unsigned char state;
 
 void setState(state);
@@ -75,7 +78,7 @@ unsigned int score_scores[PS_SCORE_COUNT] = {
 	
 	385,
 	124,
-	42
+	0
 };
 
 // State dependent variables live in this struct
@@ -99,6 +102,11 @@ union Variables {
 	} over;
 	
 	struct High {
+		char * name;
+		unsigned char selected;
+		unsigned char blink;
+		unsigned int scorePos;
+		unsigned int lastSelect;
 	} high;
 	
 	struct Score {
@@ -118,17 +126,6 @@ const unsigned int speed_scales[] = {
 	4,
 	2,
 	1
-};
-
-const unsigned char speed_leds[] = {
-	0x80,
-	0xC0,
-	0xE0,
-	0xF0,
-	0xF8,
-	0xFC,
-	0xFE,
-	0xFF
 };
 
 // ===================
@@ -165,32 +162,34 @@ int main() {
 	return 0;
 }
 
+// =======================
+// === Different loops ===
+// =======================
+
 void loop_menu() {
 	unsigned char buttons = input_getButtons();
 	
-	if (buttons)
+	if (buttons) 
 		usedButtons = buttons;
 	
-	if (buttons) {
+	if (buttons && ((time_tick - vars.menu.lastSelect) >= PS_INPUT_PAD)) {
 		if ((usedButtons & 1) || (usedButtons & 8)) {
-			if ((time_tick - vars.menu.lastSelect) >= 20) {
-				vars.menu.selected = !vars.menu.selected;
-				
-				if (vars.menu.selected) {
-					display_clearRect(PSD_DISPLAY_WIDTH / 2 - 32, 20, 8, 8);
-					display_putChar(PSD_DISPLAY_WIDTH / 2 - 24, 4, PSF_CHAR_SELECTOR_LEFT);
-				} else {
-					display_clearRect(PSD_DISPLAY_WIDTH / 2 - 24, 4, 8, 8);
-					display_putChar(PSD_DISPLAY_WIDTH / 2 - 32, 20, PSF_CHAR_SELECTOR_LEFT);
-				}
-				
-				display_showRect(PSD_DISPLAY_WIDTH / 2 - 32, 20, 8, 8);
-				display_showRect(PSD_DISPLAY_WIDTH / 2 - 24, 4, 8, 8);
-				
-				vars.menu.lastSelect = time_tick;
+			if (vars.menu.selected) {
+				display_clearRect(PSD_DISPLAY_WIDTH / 2 - 32, 20, 8, 8);
+				display_putChar(PSD_DISPLAY_WIDTH / 2 - 24, 4, PSF_CHAR_SELECTOR_LEFT);
+			} else {
+				display_clearRect(PSD_DISPLAY_WIDTH / 2 - 24, 4, 8, 8);
+				display_putChar(PSD_DISPLAY_WIDTH / 2 - 32, 20, PSF_CHAR_SELECTOR_LEFT);
 			}
+			
+			display_showRect(PSD_DISPLAY_WIDTH / 2 - 32, 20, 8, 8);
+			display_showRect(PSD_DISPLAY_WIDTH / 2 - 24, 4, 8, 8);
+			
+			
+			vars.menu.selected = !vars.menu.selected;
+			vars.menu.lastSelect = time_tick;
 		} else {
-			setState((vars.menu.selected) ? PS_STATE_GAME : PS_STATE_MENU);
+			setState((vars.menu.selected) ? PS_STATE_SCORE : PS_STATE_GAME);
 		}
 	}
 	
@@ -207,7 +206,7 @@ void loop_game() {
 		return;
 	
 	vars.game.speed = input_getDial() / 128;
-	PORTE = speed_leds[vars.game.speed];
+	PORTE = 255 - (127 >> vars.game.speed);
 	
 	lastTime = time_tick;
 	
@@ -249,36 +248,26 @@ void loop_over() {
 	if (time_tick == lastTime)
 		return;
 	
-	if (time_tick - stateBeginTime <= 160) {
-		if (((time_tick - stateBeginTime) % 20) == 0) {
+	if (time_tick - stateBeginTime <= 140) {
+		// Play a simple animation
+		if (((time_tick - stateBeginTime) % PS_BLINK_PERIOD) == 0) {
 			display_invert();
 			display_show();
 		}
 	} else {
-		if (vars.over.substate) {
-			if (buttons) {
-				if ((buttons & 1) || (buttons & 8)) {
-					if ((time_tick - vars.over.lastSelect) >= 20) {
-						vars.over.selected = !vars.over.selected;
-						
-						if (vars.over.selected) {
-							display_clearRect(PSD_DISPLAY_WIDTH / 2 - 28, 12, 8, 8);
-							display_putChar(PSD_DISPLAY_WIDTH / 2 - 24, 22, PSF_CHAR_SELECTOR_LEFT);
-						} else {
-							display_clearRect(PSD_DISPLAY_WIDTH / 2 - 24, 22, 8, 8);
-							display_putChar(PSD_DISPLAY_WIDTH / 2 - 28, 12, PSF_CHAR_SELECTOR_LEFT);
-						}
-						
-						display_showRect(PSD_DISPLAY_WIDTH / 2 - 24, 22, 8, 8);
-						display_showRect(PSD_DISPLAY_WIDTH / 2 - 28, 12, 8, 8);
-						
-						vars.over.lastSelect = time_tick;
-					}
-				} else {
-					setState((vars.over.selected) ? PS_STATE_MENU : PS_STATE_GAME);
+		// This will make sure we only update the screen once
+		if (!vars.over.substate) {
+			int i;
+			// Check if the new score is a highscore
+			for (i = 0; i < PS_SCORE_COUNT; i++)
+				if (score_scores[i] < game_score) {
+					// Cheat big time
+					vars.high.name = score_names[i];
+					vars.high.scorePos = i;
+					setState(PS_STATE_HIGH);
+					return;
 				}
-			}
-		} else {
+			// Show the actual screen
 			display_clear();
 			int size;
 			char * str = intToStr(game_score, &size);
@@ -289,21 +278,168 @@ void loop_over() {
 			vars.over.selected = 0;
 			display_show();
 			vars.over.substate = 1;
+		} else {
+			// Respont to input
+			if (buttons && ((time_tick - vars.over.lastSelect) >= PS_INPUT_PAD)) {
+				if ((usedButtons & 1) || (usedButtons & 8)) {
+					vars.over.selected = !(vars.over.selected);
+					
+					if (vars.over.selected) {
+						display_clearRect(PSD_DISPLAY_WIDTH / 2 - 28, 12, 8, 8);
+						display_putChar(PSD_DISPLAY_WIDTH / 2 - 24, 22, PSF_CHAR_SELECTOR_LEFT);
+					} else {
+						display_clearRect(PSD_DISPLAY_WIDTH / 2 - 24, 22, 8, 8);
+						display_putChar(PSD_DISPLAY_WIDTH / 2 - 28, 12, PSF_CHAR_SELECTOR_LEFT);
+					}
+					
+					display_showRect(PSD_DISPLAY_WIDTH / 2 - 24, 22, 8, 8);
+					display_showRect(PSD_DISPLAY_WIDTH / 2 - 28, 12, 8, 8);
+					
+					vars.over.lastSelect = time_tick;
+				} else {
+					setState((vars.over.selected) ? PS_STATE_MENU : PS_STATE_GAME);
+				}
+			}
+			
 		}
 	}
 	
 	lastTime = time_tick;
 }
 
+void showSelection() {
+	switch (vars.high.selected) {
+	case 0:
+		display_putChar((PSD_DISPLAY_WIDTH - (3 * PSF_CHAR_WIDTH)) / 2, 16, vars.high.name[vars.high.selected]);
+		display_showRect((PSD_DISPLAY_WIDTH - (3 * PSF_CHAR_WIDTH)) / 2, 16, PSF_CHAR_WIDTH, PSF_CHAR_HEIGHT);
+	break;
+	
+	case 1:
+		display_putChar((PSD_DISPLAY_WIDTH - PSF_CHAR_WIDTH) / 2, 16, vars.high.name[vars.high.selected]);
+		display_showRect((PSD_DISPLAY_WIDTH - PSF_CHAR_WIDTH) / 2, 16, PSF_CHAR_WIDTH, PSF_CHAR_HEIGHT);
+	break;
+	
+	case 2:
+		display_putChar((PSD_DISPLAY_WIDTH + PSF_CHAR_WIDTH) / 2, 16, vars.high.name[vars.high.selected]);
+		display_showRect((PSD_DISPLAY_WIDTH + PSF_CHAR_WIDTH) / 2, 16, PSF_CHAR_WIDTH, PSF_CHAR_HEIGHT);
+	break;
+	
+	default:
+		display_putString((PSD_DISPLAY_WIDTH - (4 * PSF_CHAR_WIDTH)) / 2, 24, "Save");
+		display_showRect((PSD_DISPLAY_WIDTH - (4 * PSF_CHAR_WIDTH)) / 2, 24, PSF_CHAR_WIDTH * 4, PSF_CHAR_HEIGHT);
+	break;
+	}
+}
+
 void loop_high() {
+	unsigned char buttons = input_getButtons();
+	
+	if (buttons)
+		usedButtons = buttons;
+	
 	if (lastTime == time_tick)
 		return;
 	
-	display_clear();
-	display_putString(0, 0, "New Highscore!");
-	display_show();
-	
 	lastTime = time_tick;
+	
+	// Make sure the player has time no notice change before reading inputs
+	if (time_tick - stateBeginTime < PS_INPUT_PAD)
+		return;
+	
+	if (((time_tick - stateBeginTime) % PS_BLINK_PERIOD) == 0) {
+		switch (vars.high.selected) {
+		case 0:
+			// First letter of name
+			if (vars.high.blink)
+				display_putChar((PSD_DISPLAY_WIDTH - (3 * PSF_CHAR_WIDTH)) / 2, 16, vars.high.name[0]);
+			else
+				display_clearRect((PSD_DISPLAY_WIDTH - (3 * PSF_CHAR_WIDTH)) / 2, 16, PSF_CHAR_WIDTH, PSF_CHAR_HEIGHT);
+			display_showRect((PSD_DISPLAY_WIDTH - (3 * PSF_CHAR_WIDTH)) / 2, 16, PSF_CHAR_WIDTH, PSF_CHAR_HEIGHT);
+		break;
+		
+		case 1:
+			// Second letter of name
+			if (vars.high.blink)
+				display_putChar((PSD_DISPLAY_WIDTH - PSF_CHAR_WIDTH) / 2, 16, vars.high.name[1]);
+			else
+				display_clearRect((PSD_DISPLAY_WIDTH - PSF_CHAR_WIDTH) / 2, 16, PSF_CHAR_WIDTH, PSF_CHAR_HEIGHT);
+			display_showRect((PSD_DISPLAY_WIDTH - PSF_CHAR_WIDTH) / 2, 16, PSF_CHAR_WIDTH, PSF_CHAR_HEIGHT);
+		break;
+			
+		case 2:
+			// Third letter of name
+			if (vars.high.blink)
+				display_putChar((PSD_DISPLAY_WIDTH + PSF_CHAR_WIDTH) / 2, 16, vars.high.name[2]);
+			else
+				display_clearRect((PSD_DISPLAY_WIDTH + PSF_CHAR_WIDTH) / 2, 16, PSF_CHAR_WIDTH, PSF_CHAR_HEIGHT);
+			display_showRect((PSD_DISPLAY_WIDTH + PSF_CHAR_WIDTH) / 2, 16, PSF_CHAR_WIDTH, PSF_CHAR_HEIGHT);
+		break;
+			
+		case 3:
+			// Save
+			if (vars.high.blink)
+				display_putString((PSD_DISPLAY_WIDTH - (4 * PSF_CHAR_WIDTH)) / 2, 24, "Save");
+			else
+				display_clearRect((PSD_DISPLAY_WIDTH - (4 * PSF_CHAR_WIDTH)) / 2, 24, PSF_CHAR_WIDTH * 4, PSF_CHAR_HEIGHT);
+			display_showRect((PSD_DISPLAY_WIDTH - (4 * PSF_CHAR_WIDTH)) / 2, 24, PSF_CHAR_WIDTH * 4, PSF_CHAR_HEIGHT);
+		break;
+		};
+		
+		vars.high.blink = !(vars.high.blink);
+	}
+	
+	// Respond to input
+	if (buttons && ((time_tick - vars.high.lastSelect) >= PS_INPUT_PAD)) {
+		if (usedButtons & 8) {
+			if (vars.high.blink)
+				showSelection();
+			
+			// Go left
+			if (vars.high.selected == 0)
+				vars.high.selected = 3;
+			else
+				vars.high.selected--;
+			
+		} else if (usedButtons & 1) {
+			if (vars.high.blink)
+				showSelection();
+			
+			// Go right
+			if (vars.high.selected == 3)
+				vars.high.selected = 0;
+			else
+				vars.high.selected++;
+		} else {
+			if (vars.high.selected == 3) {
+				// Save high score
+				score_scores[vars.high.scorePos] = game_score;
+				// Name is taken care of because we dereference its position
+				setState(PS_STATE_MENU);
+				return;	// An important return
+			} else {
+				// Check which direction we're going
+				if (usedButtons & 4) {
+					// Go up
+					if (vars.high.name[vars.high.selected] == 'A')
+						vars.high.name[vars.high.selected] = 'z';
+					else if (vars.high.name[vars.high.selected] == 'a')
+						vars.high.name[vars.high.selected] = 'Z';
+					else
+						vars.high.name[vars.high.selected]--;
+				} else {
+					// Go down
+					if (vars.high.name[vars.high.selected] == 'Z')
+						vars.high.name[vars.high.selected] = 'a';
+					else if (vars.high.name[vars.high.selected] == 'z')
+						vars.high.name[vars.high.selected] = 'A';
+					else
+						vars.high.name[vars.high.selected]++;
+				}
+				showSelection();
+			}
+		}
+		vars.high.lastSelect = time_tick;
+	}	
 };
 
 void loop_score() {
@@ -329,6 +465,7 @@ void loop_idle() {
 }
 
 void setState(state s) {
+	PORTE = 0;
 	switch (s) {
 	case PS_STATE_GAME:
 		game_start(PS_SNAKE_SIZE, input_getSwitches());
@@ -336,17 +473,27 @@ void setState(state s) {
 	break;
 	
 	case PS_STATE_OVER:
-	case PS_STATE_HIGH:
 		vars.over.lastSelect = time_tick;
 		vars.over.substate = 0;
 		int i;
-		unsigned int highscore = 0;
-		for (i = PS_SCORE_COUNT; i > 0; --i)
-			if (score_scores[i] < game_score) {
-				highscore = 1;
-				break;
-			}
-		loop = (highscore) ? &loop_high : &loop_over;
+		loop = &loop_over;
+	break;
+	
+	case PS_STATE_HIGH:
+		// Show the new highscore!
+		display_clear();
+		display_putString(8, 0, "New Highscore!");
+		int size;
+		char * str = intToStr(game_score, &size);
+		display_putString((PSD_DISPLAY_WIDTH - (size * PSF_CHAR_WIDTH)) / 2, 8, str);
+		vars.high.selected = 0;
+		vars.high.blink = 0;
+		// The name has been taken care of in over
+		display_putString((PSD_DISPLAY_WIDTH - (3 * PSF_CHAR_WIDTH)) / 2, 16, vars.high.name);
+		display_putString((PSD_DISPLAY_WIDTH - (4 * PSF_CHAR_WIDTH)) / 2, 24, "Save");
+		display_show();
+		vars.high.lastSelect = time_tick;
+		loop = &loop_high;
 	break;
 	
 	case PS_STATE_SCORE:
@@ -366,7 +513,7 @@ void setState(state s) {
 		display_putChar(PSD_DISPLAY_WIDTH / 2 - 24, 4, PSF_CHAR_SELECTOR_LEFT);
 		display_show();
 		
-		vars.menu.lastSelect = 0;
+		vars.menu.lastSelect = time_tick;
 		vars.menu.selected = 0;
 		
 		loop = &loop_menu;
